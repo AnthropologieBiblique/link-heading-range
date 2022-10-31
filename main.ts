aec671f29ae3cef507624b898ccec93a60c47abf
Limezy <arnaudetpia@de-ruffray.fr>
2022-10-31 08:28:16 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Keymap, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -16,66 +16,8 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent(document, 'mouseover', (evt: PointerEvent) => {
-		//	console.log('pointer', evt);
-		//});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 
 		let postProc: MarkdownPostProcessor;
 
@@ -107,18 +49,20 @@ export default class MyPlugin extends Plugin {
 
 		let hoverHeaderRange = (event: MouseEvent, target: HTMLElement) => {
 			console.log("hoverHeaderRange")
-			this.app.workspace.trigger("link-hover",{},null, "/Target", "/Target")
+			const p = document.createElement("p");
+			p.textContent = "Hello, World!";
+			//this.app.workspace.trigger("link-hover",{},target, "Target", "")
+			this.app.workspace.trigger("hover-link",event,"header-range-link",this.parent,target, "Target", "")
 		}
 
-		let clickHeaderRange = (event: MouseEvent, target: HTMLElement) => {
-			console.log("clickHeaderRange")
-			event.preventDefault();
-        	event.stopPropagation();
-        	(event: MouseEvent, link: string, app: App) => {
-    		this.app.workspace.openLinkText("Target", '')};
+		let clickHeaderRange = async (event: MouseEvent, target: HTMLElement) => {
+    		await this.app.workspace.openLinkText(target.getAttr("href"), "/",Keymap.isModifier(event, 'Mod') || 1 === event.button)
+			// TODO : scroll and highlight in yellow the header range
 		}
 
+		// Show the header range when hovering on header-range-link
 		document.on('mouseover', `.header-range-link`, hoverHeaderRange);
+		// Copy internal-link behaviour when header-range-link is clicked
 		document.on('click', `.header-range-link`, clickHeaderRange);
 
 	}
@@ -133,22 +77,6 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
 
@@ -179,9 +107,4 @@ class SampleSettingTab extends PluginSettingTab {
 					await this.plugin.saveSettings();
 				}));
 	}
-
-	//filePreviewOnHover = (event: MouseEvent, target: HTMLElement) => {
-    	//console.log("coucou")
-    	//this.app.workspace.trigger('link-hover', {}, event.target, target.getAttr('href'), target.getAttr('href'));
-	//};
 }
